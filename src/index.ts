841e53cc4e01d1d7cc176647138de5346dc71ca7
tbosmans <tom.94.bosmans@gmail.com>
2023-09-03 15:15:30 +0200

    Add rome for linting ad formatting.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import { Hono } from "hono";
-import { serveStatic } from "hono/serve-static.bun";
+import { Hono } from "hono"
+import { serveStatic } from "hono/serve-static.bun"
 
-const port = parseInt(process.env.PORT) || 3000;
+const port = parseInt(process.env.PORT) || 3000
 
-const app = new Hono();
+const app = new Hono()
 
-app.use("/favicon.ico", serveStatic({ path: "./public/favicon.ico" }));
+app.use("/favicon.ico", serveStatic({ path: "./public/favicon.ico" }))
 
 app.get("/", (c) => {
-  return c.json({ message: "Hello World!" });
-});
+  return c.json({ message: "Hello World!" })
+})
 
-console.log(`Running at http://localhost:${port}`);
+console.log(`Running at http://localhost:${port}`)
 
 export default {
   port,
   fetch: app.fetch,
-};
+}
